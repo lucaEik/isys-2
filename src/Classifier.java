083fe4027e7bb3e62270f619af0ae0c9bcfc2f3b
Luca Thurm <luca.eik@gmail.com>
2018-11-30 00:06:35 +0100

    changed classifier and main

--- src/Classifier.java
+++ src/Classifier.java
@@ -1,3 +1,12 @@
+import org.apache.commons.lang.ArrayUtils;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.RandomForest;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Normalize;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -6,38 +15,339 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class Classifier  {
 
+    private int attrRange = 10;
 
-    private List<ArrayList<Double>> data = new ArrayList<>();
+    private double[][] data;
 
-    public Classifier(String dataPath) {
-        try {
-            // csv file with data:
-            File file = new File(dataPath);
-            BufferedReader bf = new BufferedReader(new FileReader(file));
+    private RandomForest classifier;
 
-            // read the data from the csv file:
-            String line;
-            String[] splitStrings;
-            while ((line = bf.readLine()) != null) {
-                splitStrings = line.split(",");
-                ArrayList<Double> row = new ArrayList<>();
-                for (int i = 0; i < splitStrings.length; i++) {
-                    row.add(Double.valueOf(splitStrings[i]));
-                }
-                this.data.add(row);
+    public Classifier(String dataPath) throws IOException {
+        // csv file with data:
+        File file = new File(dataPath);
+        BufferedReader bf = new BufferedReader(new FileReader(file));
+
+        // read the data from the csv file:
+        List<List<Double>> dataList = new ArrayList<>();
+        String line;
+        String[] splitStrings;
+        while ((line = bf.readLine()) != null) {
+            splitStrings = line.split(",");
+            ArrayList<Double> row = new ArrayList<>();
+            for (int i = 0; i < splitStrings.length; i++) {
+                row.add(Double.valueOf(splitStrings[i]));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            dataList.add(row);
         }
+
+        Double[][] doubs = new Double[dataList.size()][];
+        for (int i = 0; i < dataList.size(); i++) {
+            doubs[i] = dataList.get(i).toArray(new Double[dataList.get(i).size()]);
+        }
+
+        double[][] data = new double[doubs.length][];
+        for (int i = 0; i < doubs.length; i++) {
+            data[i] = ArrayUtils.toPrimitive(doubs[i]);
+        }
+        this.data = data;
     }
 
+    public DataObject[] readObjects(String path) throws IOException {
+        File file = new File(path);
+        BufferedReader bf = new BufferedReader(new FileReader(file));
+
+        String line;
+        String[] split;
+
+        // List for the objects from the csv file
+        List<DataObject> objects = new ArrayList<>();
+
+        // read the data from file
+        while ((line = bf.readLine()) != null) {
+            split = line.split(",");
+            objects.add(new DataObject(Integer.valueOf(split[1]), Integer.valueOf(split[0])));
+        }
+
+        return objects.toArray(new DataObject[objects.size()]);
+    }
+
+    private double[] nearestKNeighbours(DataObject obj, int range, int steps) {    //TODO: maybe add step size
+        // TODO fix the "out of range problem"
+        double[] neighbours = new double[(int)Math.pow(2 * (range + 1), 2)];
+        int index = 0;
+        for (int row = -range; row <= range; row+=steps) {
+            for (int col = -range; col <= range; col+=steps, index++) {
+                if (obj.getRow() + row >= 0 && obj.getCol() + col >= 0 && obj.getRow() + row < data.length && obj.getCol() + col < data[0].length) {
+                    neighbours[index] = data[obj.getRow() + row][obj.getCol() + col];
+                } else {
+                    neighbours[index] = data[obj.getRow()][obj.getCol()];   // TODO: object markers value is added to prevent OOB-exception
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    private double avgHeightDifference(DataObject obj, int range) {
+        // TODO fix the "out of range problem"
+        double avgHeight = 0;
+        int counter = 0;
+        for (int row = -range; row <= range; row++) {
+            for (int col = -range; col <= range; col++, counter++) {
+                if (obj.getRow() + row >= 0 && obj.getCol() + col >= 0 && obj.getRow() + row < data.length && obj.getCol() + col < data[0].length) {
+                    avgHeight += data[obj.getRow() + row][obj.getCol() + col];
+                }
+            }
+        }
+
+        return Math.abs(data[obj.getRow()][obj.getCol()] - (avgHeight / counter));
+    }
+
+    /*
+    private double rise(DataObject obj, int range) {
+        // TODO fix the "out of range problem"
+
+        if (obj.getRow() + range >= 0 && obj.getCol() + range >= 0 && obj.getRow() + range < data.length && obj.getCol() + range < data[0].length)
+            return 0;
+
+        double north, south, east, west;
+        north = Math.abs(data[obj.getRow()][obj.getCol()] - data[obj.getRow() - range][obj.getCol()]) / range;
+        south = Math.abs(data[obj.getRow()][obj.getCol()] - data[obj.getRow() + range][obj.getCol()]) / range;
+        east = Math.abs(data[obj.getRow()][obj.getCol()] - data[obj.getRow()][obj.getCol() + range]) / range;
+        west = Math.abs(data[obj.getRow()][obj.getCol()] - data[obj.getRow()][obj.getCol() - range]) / range;
+
+        return (north + east + south + west) / 4;
+    }
+    */
+
+    public void trainNew(String aObjectsPath, String bObjectsPath) throws Exception{
+        // Lists for the training objects
+        DataObject[] aObjects = readObjects(aObjectsPath);
+        DataObject[] bObjects = readObjects(bObjectsPath);
+
+        // height differences
+        int bothLen = aObjects.length + bObjects.length;
+        double[][] values = new double[bothLen][1];
+        for (int i = 0; i < aObjects.length; i++) {
+            values[i][0] = avgHeightDifference(aObjects[i], attrRange);
+        }
+        for (int i = aObjects.length; i < bothLen; i++) {
+            values[i][0] = avgHeightDifference(bObjects[i - aObjects.length], attrRange);
+        }
+
+        // classes
+        List<String> targets = new ArrayList<>();
+        targets.add("A");
+        targets.add("B");
+
+        // add attribute names
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("avgHeightDiff"));
+
+
+        // create instances with attributes etc.
+        Instances dataRaw = new Instances("TrainInstance", attributes, 0);
+
+        // add attribute values
+        for (int i = 0; i < values.length; i++) {
+            dataRaw.add(new DenseInstance(1.0, values[i]));
+        }
+
+        // insert target attribute
+        dataRaw.insertAttributeAt(new Attribute("target", targets), dataRaw.numAttributes());
+
+        // set target value for A-objects
+        for (int i = 0 ; i < aObjects.length ; i++) {
+            dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "A");
+        }
+
+        // set target values for B-objects
+        for (int i = aObjects.length; i < values.length; i++) {
+            dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "B");
+        }
+
+        // set class index to target attribute (last attribute of the Instances)
+        dataRaw.setClassIndex(dataRaw.numAttributes() - 1);
+
+        dataRaw.randomize(new Random(1));
+
+        int trainSize = (int) Math.round(dataRaw.numInstances() * 0.8);
+        int testSize = dataRaw.numInstances() - trainSize;
+        Instances testData = new Instances(dataRaw, 0, testSize);
+        Instances trainData = new Instances(dataRaw, testSize, trainSize);
+
+        int folds = 5;
+
+        Evaluation eval = new Evaluation(testData);
+
+        // build random forest classifier
+        RandomForest clf = new RandomForest();
+        clf.buildClassifier(trainData);
+
+        eval.crossValidateModel(clf, testData, folds, new Random(1));
+
+        System.out.println(eval.toMatrixString());
+        System.out.println(eval.toSummaryString("\nResults\n========\n", false));
+        System.out.println("Results for B");
+        System.out.println("precision= " + eval.precision(0));
+        System.out.println("recall= " + eval.recall(0));
+        System.out.println("Results for A");
+        System.out.println("precision= " + eval.precision(1));
+        System.out.println("recall= " + eval.recall(1));
+
+        // save the random forest in the classifier field
+        this.classifier = clf;
+
+}
+/*
+    public void train(String aObjectsPath, String bObjectsPath) throws Exception {
+
+        // Lists for the training objects
+        DataObject[] aObjects = readObjects(aObjectsPath);
+        DataObject[] bObjects = readObjects(bObjectsPath);
+
+        System.out.println(aObjects.length);
+        System.out.println(bObjects.length);
+
+        // neighbours
+        int neighboursLen = aObjects.length + bObjects.length;
+        double[][] neighbours = new double[neighboursLen][];
+        for (int i = 0; i < aObjects.length; i++) {
+            neighbours[i] = nearestKNeighbours(aObjects[i], 5, 1);   //TODO range
+        }
+        for (int i = aObjects.length; i < neighboursLen; i++) {
+            neighbours[i] = nearestKNeighbours(bObjects[i - aObjects.length], 5, 1);   //TODO range
+        }
+
+        //classes
+        List<String> targets = new ArrayList<>();
+        targets.add("A");
+        targets.add("B");
+
+        //add attribute names
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        for (int i = 0; i < neighbours[0].length; i++) {
+            attributes.add(new Attribute(String.valueOf(i)));
+        }
+
+        // create instances with attributes etc.
+        Instances dataRaw = new Instances("TrainInstance", attributes, 0);
+
+        // add attribute values
+        for (int i = 0; i < neighbours.length; i++) {
+            dataRaw.add(new DenseInstance(1.0, neighbours[i]));
+        }
+
+        // insert target attribute
+        dataRaw.insertAttributeAt(new Attribute("target", targets), dataRaw.numAttributes());
+
+        // set target value for A-objects
+        for (int i = 0 ; i < aObjects.length ; i++) {
+            dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "A");
+        }
+
+        // set target values for B-objects
+        for (int i = aObjects.length; i < neighbours.length; i++) {
+            dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "B");
+        }
+
+        // set class index to target attribute (last attribute of the Instances)
+        dataRaw.setClassIndex(dataRaw.numAttributes() - 1);
+
+        dataRaw.randomize(new Random(1));
+        Filter normalizer = new Normalize();
+        normalizer.setInputFormat(dataRaw);
+        Instances normalized = Filter.useFilter(dataRaw, normalizer);
+
+        int trainSize = (int) Math.round(dataRaw.numInstances() * 0.8);
+        int testSize = dataRaw.numInstances() - trainSize;
+        Instances testData = new Instances(normalized, 0, testSize);
+        Instances trainData = new Instances(normalized, testSize, trainSize);
+
+        int folds = 5;
+
+        Evaluation eval = new Evaluation(testData);
+
+        // build random forest classifier
+        RandomForest clf = new RandomForest();
+        clf.buildClassifier(trainData);
+
+        eval.crossValidateModel(clf, testData, folds, new Random(1));
+
+        System.out.println(eval.toMatrixString());
+        System.out.println(eval.toSummaryString("\nResults\n========\n", false));
+        System.out.println("Results for B");
+        System.out.println("precision= " + eval.precision(0));
+        System.out.println("recall= " + eval.recall(0));
+        System.out.println("Results for A");
+        System.out.println("precision= " + eval.precision(1));
+        System.out.println("recall= " + eval.recall(1));
+
+        /*
+        dataRaw.randomize(new Debug.Random(1));
+        Filter normalizer = new Normalize();
+
+        normalizer.setInputFormat(dataRaw);
+        Instances normalized = Filter.useFilter(dataRaw, normalizer);
+
+        int folds = 5;
+        RandomForest clf = new RandomForest();
+
+        // save the random forest in the classifier field
+        this.classifier = clf;
+    }
+*/
+    public Map<String, List<DataObject>> classify(String objectsPath) throws Exception {
+        // analog zu train()
+        DataObject[] objects = readObjects(objectsPath);
+        double[][] values = new double[objects.length][2];
+        for (int i = 0; i < objects.length; i++) {
+            values[i][0] = avgHeightDifference(objects[i], attrRange);
+            //values[i][1] = rise(objects[i], attrRange);
+        }
+        List<String> targets = new ArrayList<>();
+        targets.add("A");
+        targets.add("B");
+
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("avgHeightDiff"));
+
+        Instances dataRaw = new Instances("DataInstance", attributes, 0);
+        for (int i = 0; i < values.length; i++) {
+            dataRaw.add(new DenseInstance(1.0, values[i]));
+        }
+
+        // insert target attribute
+        dataRaw.insertAttributeAt(new Attribute("target", targets), dataRaw.numAttributes());
+
+        // set class index to target attribute
+        dataRaw.setClassIndex(dataRaw.numAttributes() - 1);
+
+        Map<String, List<DataObject>> results = new HashMap<>();
+        results.put("A", new ArrayList<>());
+        results.put("B", new ArrayList<>());
+
+        for (int i = 0; i < dataRaw.numInstances(); i++) {
+            if (this.classifier.classifyInstance(dataRaw.instance(i)) == 1.0) {
+                results.get("B").add(objects[i]);
+            } else if (this.classifier.classifyInstance(dataRaw.instance(i)) == 0.0) {
+                results.get("A").add(objects[i]);
+            }
+        }
+
+        return results;
+    }
+
+    /*
     public List<ArrayList<Double>> getData(){
         return this.data;
     }
+    */
 
+    /*
     public List<DataObject> dataPoints(String path) throws  IOException {
 
         File file = new File(path);
@@ -56,7 +366,8 @@ public class Classifier  {
         }
         return objects;
     }
-
+    */
+    /*
     public ArrayList<ArrayList<Double>> nearestKNeightbours(int range,int steps, List<DataObject> datapoints) {
         int min_range = -range;
         int max_range = range;
@@ -68,9 +379,12 @@ public class Classifier  {
             int y_index = datapoints.get(i).getCol();
             for (int j = min_range; j <= max_range; j+= steps) {
                 for (int z = min_range; z <= max_range; z+= steps) {
-                    if (x_index + j < 0 || x_index + j >= 4943 || y_index + z < 0 || y_index + z >= 3000 )
-                    {rangelist.add(0.0);}
-                    else {rangelist.add(this.data.get(x_index + j).get(y_index + z));}
+                    if (x_index + j < 0 || x_index + j >= 4943 || y_index + z < 0 || y_index + z >= 3000 ) {
+                        rangelist.add(0.0);
+                    }
+                    else {
+                        rangelist.add(this.data.get(x_index + j).get(y_index + z));
+                    }
                 }
             }
             //  rangelist.remove(this.data.get(x_index).get(y_index));
@@ -79,13 +393,13 @@ public class Classifier  {
         }
         return ret;
     }
-
-
+    */
+    /*
     public ArrayList<ArrayList<Double>> concatenateAB(ArrayList<ArrayList<Double>>A, ArrayList<ArrayList<Double>> B) {
         for (ArrayList<Double> list :  B) {
             A.add(list);
         }
         return A;
     }
-
+    */
 }
--- src/Main.java
+++ src/Main.java
@@ -26,29 +26,47 @@ public class Main {
 
     public static void main(String args[]) throws IOException, Exception {
 
+        Classifier c = new Classifier("data/data.csv");
+        c.trainNew("data/A0.csv", "data/B0.csv");
 
-        String data = System.getProperty("user.dir") + "/data.csv";
-        String A0 = System.getProperty("user.dir") + "/A0.csv";
-        String B0 = System.getProperty("user.dir") + "/B0.csv";
-        String A1 = System.getProperty("user.dir") + "/A1.csv";
-        String B1 = System.getProperty("user.dir") + "/B1.csv";
+        /*
+        System.out.println("Results A1: ");
+        Map<String, List<DataObject>> results = c.classify("data/A1.csv");
+        System.out.println("A: " + results.get("A").size());
+        System.out.println("B: " + results.get("B").size());
+
+        System.out.println("Results B1:");
+        results = c.classify("data/B1.csv");
+        System.out.println("A: " + results.get("A").size());
+        System.out.println("B: " + results.get("B").size());
+        */
+        /*
+
+        String data = System.getProperty("user.dir") + "/data/data.csv";
+        String A0 = System.getProperty("user.dir") + "/data/A0.csv";
+        String B0 = System.getProperty("user.dir") + "/data/B0.csv";
+        //String A1 = System.getProperty("user.dir") + "/A1.csv";
+        //String B1 = System.getProperty("user.dir") + "/B1.csv";
         Classifier c = new Classifier(data);
 
         List<DataObject> A_obj = c.dataPoints(A0);
         List<DataObject> B_obj = c.dataPoints(B0);
 
 
+        // array with attributes
         ArrayList<ArrayList<Double>> A_x = c.nearestKNeightbours(10,2, A_obj);
         ArrayList<ArrayList<Double>> B_x = c.nearestKNeightbours(10,2, B_obj);
 
         ArrayList<ArrayList<Double>> A_B_Array = c.concatenateAB(A_x, B_x);
 
+        // convert 2d list to 2d array
         Double[][] doubleArray = new Double[A_B_Array.size()][];
         for (int i = 0; i < A_B_Array.size(); i++) {
             List<Double> row = A_B_Array.get(i);
             doubleArray[i] = row.toArray(new Double[row.size()]);
         }
 
+        // classes:
         List<String> targets = new LinkedList<>();
         targets.add("B");
         targets.add("A");
@@ -57,13 +75,13 @@ public class Main {
 
 
 
-
+        // add attribute names to attribute list
         ArrayList<Attribute> attributes = new ArrayList<>();
         for (int i = 0; i < doubleArray[0].length; i++) {
             attributes.add(new Attribute(String.valueOf(i)));
         }
 
-
+        // target attribute
         Attribute classAttr = new Attribute("target", targets);
 
         Instances dataRaw = new Instances("Datainstance", attributes, 0);
@@ -76,10 +94,12 @@ public class Main {
         dataRaw.insertAttributeAt(classAttr, dataRaw.numAttributes());
 
 
+        // set target value for A-objects
         for (int i = 0 ; i < A_obj.size(); i++) {
             dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "A");
         }
 
+        // set target values for B-objects
         for (int i = A_obj.size(); i < A_B_Array.size(); i++) {
             dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "B");
         }
@@ -110,6 +130,7 @@ public class Main {
 
         eval.crossValidateModel(clf, testData, folds, new Random(1));
 
+        //print results
         System.out.println(eval.toMatrixString());
         System.out.println(eval.toSummaryString("\nResults\n========\n", false));
         System.out.println("Results for B");
@@ -119,7 +140,7 @@ public class Main {
         System.out.println("precision= " + eval.precision(1));
         System.out.println("recall= " + eval.recall(1));
 
-
+        */
 
     }
 }
