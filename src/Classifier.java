4840c1fbdb7b9a4f8791c5b833f35814fa68296f
Luca Thurm <luca.eik@gmail.com>
2018-11-29 11:08:05 +0100

    changed to weka

--- src/Classifier.java
+++ src/Classifier.java
@@ -7,10 +7,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Classifier {
+public class Classifier  {
 
 
-    private List<List<Double>> data = new ArrayList<>();
+    private List<ArrayList<Double>> data = new ArrayList<>();
 
     public Classifier(String dataPath) {
         try {
@@ -34,9 +34,13 @@ public class Classifier {
         }
     }
 
-    public Map<Character, List<DataObject>> classify(String objectsPath, int fieldSize, double avgDiffThreshold) throws IOException{
-        //csv file with data:
-        File file = new File(objectsPath);
+    public List<ArrayList<Double>> getData(){
+        return this.data;
+    }
+
+    public List<DataObject> dataPoints(String path) throws  IOException {
+
+        File file = new File(path);
         BufferedReader bf = new BufferedReader(new FileReader(file));
 
         String line;
@@ -50,129 +54,38 @@ public class Classifier {
             split = line.split(",");
             objects.add(new DataObject(Integer.valueOf(split[1]), Integer.valueOf(split[0])));
         }
-
-        // as is going contain the objects classified as A, bs is going to ccntains the objects classified as B
-        List<DataObject> as = new ArrayList<>();
-        List<DataObject> bs = new ArrayList<>();
-
-        double avg = 0;
-
-        // if there's a "cliff" in the area around the data object, it is classified as B, if there's none, as A
-        for (DataObject obj : objects) {
-            double diff = highestStsDiff(obj, fieldSize);
-            avg += diff;
-            if (diff > avgDiffThreshold) {
-                bs.add(obj);
-            } else {
-                as.add(obj);
-            }
-        }
-
-        System.out.println(avg / objects.size());
-
-        // the map for the results of this classification. The key 'A' accesses the objects classified as A
-        // and the key 'B' the B-objects
-        Map<Character, List<DataObject>> results = new HashMap<>();
-        results.put('A', as);
-        results.put('B', bs);
-
-        return results;
+        return objects;
     }
 
-    private double highestStsDiff(DataObject obj, int fieldSize){
-        //TODO: find a solution for out of bounds problem
-        if (obj.getCol() - fieldSize - 1 < 0 || obj.getRow() - fieldSize - 1 < 0) return 1.9;
-        if (obj.getCol() + fieldSize + 1 >= data.get(0).size() || obj.getRow() + fieldSize + 1 >= data.size()) return 1.9;
+    public ArrayList<ArrayList<Double>> nearestKNeightbours(int range,int steps, List<DataObject> datapoints) {
+        int min_range = -range;
+        int max_range = range;
+        ArrayList<Double> rangelist = new ArrayList<>();
+        ArrayList<ArrayList<Double>> ret = new ArrayList<>();
 
-        // List for all the cells in the area around obj
-        List<DataObject> objects = new ArrayList<>();
-
-        // this variable will hold the highest difference between two neighbouring values in this sub-table
-        double highestDiff = 0;
-
-        for (int row = -fieldSize; row <= fieldSize; row++) {
-            for (int col = -fieldSize; col <= fieldSize; col++) {
-                objects.add(new DataObject(obj.getRow() + row, obj.getCol() + col));
-            }
-        }
-
-        // check for each cell if the neighouring cells have a much lower or higher value
-        for (DataObject o : objects) {
-            double north = Math.abs(
-                    data.get(o.getRow()).get(o.getCol())
-                            - data.get(o.getRow() - 1).get(o.getCol())
-            );
-            double south = Math.abs(
-                    data.get(o.getRow()).get(o.getCol())
-                            - data.get(o.getRow() + 1).get(o.getCol())
-            );
-            double west = Math.abs(
-                    data.get(o.getRow()).get(o.getCol())
-                            - data.get(o.getRow()).get(o.getCol() - 1)
-            );
-            double east = Math.abs(
-                    data.get(o.getRow()).get(o.getCol())
-                            - data.get(o.getRow()).get(o.getCol() + 1)
-            );
-
-            // set highestDiff to that difference value if it is higher than the value currently assigned to highestDiff
-            if (east > highestDiff) {
-                highestDiff = east;
-            }
-            if (west > highestDiff) {
-                highestDiff = west;
-            }
-            if (north > highestDiff) {
-                highestDiff = north;
-            }
-            if (south > highestDiff) {
-                highestDiff = south;
-            }
-        }
-        // the return value is the highest difference between two neighbouring cells in the area
-        return highestDiff;
-    }
-
-    /*
-    //TODO: get rid of this
-    public void printDataObject(DataObject obj, int fieldSize) {
-
-        if (!(obj.getCol() - fieldSize < 0 || obj.getRow() - fieldSize < 0)) {
-
-            double diff = 0;
-            for (int row = -fieldSize; row <= fieldSize; row++) {
-                for (int col = -fieldSize; col <= fieldSize; col++) {
-                    System.out.print(data.get(obj.getRow() + row).get(obj.getCol() + col).intValue());
-                    System.out.print(" ");
-                    if (Math.abs(data.get(obj.getRow()).get(obj.getCol()) - data.get(obj.getRow() + row).get(obj.getCol() + col)) > diff) {
-                        diff = Math.abs(data.get(obj.getRow()).get(obj.getCol()) - data.get(obj.getRow() + row).get(obj.getCol() + col));
-                    }
+        for (int i = 0; i < datapoints.size(); i++) {
+            int x_index = datapoints.get(i).getRow();
+            int y_index = datapoints.get(i).getCol();
+            for (int j = min_range; j <= max_range; j+= steps) {
+                for (int z = min_range; z <= max_range; z+= steps) {
+                    if (x_index + j < 0 || x_index + j >= 4943 || y_index + z < 0 || y_index + z >= 3000 )
+                    {rangelist.add(0.0);}
+                    else {rangelist.add(this.data.get(x_index + j).get(y_index + z));}
                 }
-                System.out.print("\n");
             }
-            System.out.println("DataPoint at " + obj.getRow() + ", " + obj.getCol());
-            System.out.println("Value: " + data.get(obj.getRow()).get(obj.getCol()));
-            System.out.println("highest Diff: " + diff);
-            System.out.println("Highest sts diff:" + highestStsDiff(obj, fieldSize) + "\n");
+            //  rangelist.remove(this.data.get(x_index).get(y_index));
+            ret.add(rangelist);
+            rangelist = new ArrayList<>();
         }
+        return ret;
     }
 
-    //NOT ACCURATE ENOUGH
-    private double avgDiff(DataObject obj, int fieldSize) {
-        //TODO: handle out of bounds exceptions
 
-        if (obj.getCol() - fieldSize < 0 || obj.getRow() - fieldSize < 0) return 1.9;
-
-        double diff = 0;
-        int count = 0;
-        for (int row = -fieldSize; row <= fieldSize; row++) {
-            for (int col = -fieldSize; col <= fieldSize; col++) {
-                count++;
-                diff += Math.abs(data.get(obj.getRow()).get(obj.getCol()) - data.get(obj.getRow() + row).get(obj.getCol() + col));
-            }
+    public ArrayList<ArrayList<Double>> concatenateAB(ArrayList<ArrayList<Double>>A, ArrayList<ArrayList<Double>> B) {
+        for (ArrayList<Double> list :  B) {
+            A.add(list);
         }
-        return diff / count;
+        return A;
     }
 
-    */
 }
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,125 @@
+import org.apache.commons.lang.ArrayUtils;
+import weka.classifiers.Evaluation;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+import java.io.IOException;
+import java.util.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+
+import weka.core.converters.CSVLoader;
+import weka.core.Attribute;
+import weka.classifiers.trees.RandomForest;
+import weka.core.Instance;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.Normalize;
+import weka.filters.unsupervised.attribute.Add;
+import weka.core.Debug;
+import weka.filters.supervised.attribute.AttributeSelection;
 
 public class Main {
 
-    public static void main(String args[]) {
-        try {
-            Classifier c = new Classifier("C:\\Users\\Luca\\Studium\\Intelligente Systeme\\Praktikum\\Aufgabe 2\\data.csv");
-
-            System.out.println("A0:");
-            Map<Character, List<DataObject>> a0Results = c.classify("C:\\Users\\Luca\\Studium\\Intelligente Systeme\\Praktikum\\Aufgabe 2\\A0.csv", 5, 10);
-            System.out.println("A-Objects: " + a0Results.get('A').size() + "(" + (double)a0Results.get('A').size() / (a0Results.get('A').size() + a0Results.get('B').size()) * 100 + "%)");
-            System.out.println("B-Objects: " + a0Results.get('B').size() + "(" + (double)a0Results.get('B').size() / (a0Results.get('A').size() + a0Results.get('B').size()) * 100 + "%)");
+    public static void main(String args[]) throws IOException, Exception {
 
 
-            System.out.println("\nB0:");
-            Map<Character, List<DataObject>> b0Results = c.classify("C:\\Users\\Luca\\Studium\\Intelligente Systeme\\Praktikum\\Aufgabe 2\\B0.csv", 5, 10);
-            System.out.println("A-Objects: " + b0Results.get('A').size() + "(" + (double)b0Results.get('A').size() / (b0Results.get('A').size() + b0Results.get('B').size()) * 100 + "%)");
-            System.out.println("B-Objects: " + b0Results.get('B').size() + "(" + (double)b0Results.get('B').size() / (b0Results.get('A').size() + b0Results.get('B').size()) * 100+ "%)");
+        String data = System.getProperty("user.dir") + "/data.csv";
+        String A0 = System.getProperty("user.dir") + "/A0.csv";
+        String B0 = System.getProperty("user.dir") + "/B0.csv";
+        String A1 = System.getProperty("user.dir") + "/A1.csv";
+        String B1 = System.getProperty("user.dir") + "/B1.csv";
+        Classifier c = new Classifier(data);
 
-            System.out.println("\nA1");
-            Map<Character, List<DataObject>> a1Results = c.classify("C:\\Users\\Luca\\Studium\\Intelligente Systeme\\Praktikum\\Aufgabe 2\\A1.csv", 5, 10);
-            System.out.println("A-Objects: " + a1Results.get('A').size() + "(" + (double)a0Results.get('A').size() / (a0Results.get('A').size() + a0Results.get('B').size()) * 100 + "%)");
-            System.out.println("B-Objects: " + a1Results.get('B').size() + "(" + (double)a0Results.get('B').size() / (a0Results.get('A').size() + a0Results.get('B').size()) * 100 + "%)");
+        List<DataObject> A_obj = c.dataPoints(A0);
+        List<DataObject> B_obj = c.dataPoints(B0);
 
 
-            System.out.println("\nB1:");
-            Map<Character, List<DataObject>> b1Results = c.classify("C:\\Users\\Luca\\Studium\\Intelligente Systeme\\Praktikum\\Aufgabe 2\\B1.csv", 5, 10);
-            System.out.println("A-Objects: " + b1Results.get('A').size() + "(" + (double)b1Results.get('A').size() / (b1Results.get('A').size() + b1Results.get('B').size()) * 100 + "%)");
-            System.out.println("B-Objects: " + b1Results.get('B').size() + "(" + (double)b1Results.get('B').size() / (b1Results.get('A').size() + b1Results.get('B').size()) * 100+ "%)");
+        ArrayList<ArrayList<Double>> A_x = c.nearestKNeightbours(10,2, A_obj);
+        ArrayList<ArrayList<Double>> B_x = c.nearestKNeightbours(10,2, B_obj);
 
-        } catch (IOException e){
-            e.printStackTrace();
+        ArrayList<ArrayList<Double>> A_B_Array = c.concatenateAB(A_x, B_x);
+
+        Double[][] doubleArray = new Double[A_B_Array.size()][];
+        for (int i = 0; i < A_B_Array.size(); i++) {
+            List<Double> row = A_B_Array.get(i);
+            doubleArray[i] = row.toArray(new Double[row.size()]);
         }
+
+        List<String> targets = new LinkedList<>();
+        targets.add("B");
+        targets.add("A");
+
+        System.out.println("Anzahl X-Merkmale: " + doubleArray[0].length );
+
+
+
+
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        for (int i = 0; i < doubleArray[0].length; i++) {
+            attributes.add(new Attribute(String.valueOf(i)));
+        }
+
+
+        Attribute classAttr = new Attribute("target", targets);
+
+        Instances dataRaw = new Instances("Datainstance", attributes, 0);
+
+        for(Double[] row : doubleArray) {
+                dataRaw.add(new DenseInstance(1.0, ArrayUtils.toPrimitive(row)));
+        }
+
+
+        dataRaw.insertAttributeAt(classAttr, dataRaw.numAttributes());
+
+
+        for (int i = 0 ; i < A_obj.size(); i++) {
+            dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "A");
+        }
+
+        for (int i = A_obj.size(); i < A_B_Array.size(); i++) {
+            dataRaw.instance(i).setValue(dataRaw.numAttributes()-1, "B");
+        }
+
+
+
+        dataRaw.setClassIndex(dataRaw.numAttributes()-1);
+
+        int trainSize = (int) Math.round(dataRaw.numInstances() * 0.8);
+        int testSize = dataRaw.numInstances() - trainSize;
+
+        dataRaw.randomize(new Debug.Random(1));
+        Filter normalizer = new Normalize();
+
+        normalizer.setInputFormat(dataRaw);
+        Instances normalized = Filter.useFilter(dataRaw, normalizer);
+
+        int folds = 5;
+        RandomForest clf = new RandomForest();
+
+
+
+        Instances testData = new Instances(normalized, 0, testSize);
+        Instances trainData = new Instances(normalized, testSize, trainSize);
+        Evaluation eval = new Evaluation(testData);
+
+        clf.buildClassifier(trainData);
+
+        eval.crossValidateModel(clf, testData, folds, new Random(1));
+
+        System.out.println(eval.toMatrixString());
+        System.out.println(eval.toSummaryString("\nResults\n========\n", false));
+        System.out.println("Results for B");
+        System.out.println("precision= " + eval.precision(0));
+        System.out.println("recall= " + eval.recall(0));
+        System.out.println("Results for A");
+        System.out.println("precision= " + eval.precision(1));
+        System.out.println("recall= " + eval.recall(1));
+
+
+
     }
 }
