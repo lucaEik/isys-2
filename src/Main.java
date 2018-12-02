5057cfe761c28fe6b853446c39752e308e359da5
Luca Thurm <luca.eik@gmail.com>
2018-12-02 12:57:43 +0100

    added data directory and adjusted code

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
     public static void main(String args[]) throws IOException, Exception {
         // paths to the data
-        String data = "C:/Users/Luca/Desktop/data/data.csv";
-        String A0 = "C:/Users/Luca/Desktop/data/A0.csv";
-        String B0 = "C:/Users/Luca/Desktop/data/B0.csv";
-        String A1 = "C:/Users/Luca/Desktop/data/A1.csv";
-        String B1 = "C:/Users/Luca/Desktop/data/B1.csv";
+        String data = "data/data.csv";
+        String A0 = "data/A0.csv";
+        String B0 = "data/B0.csv";
+        String A1 = "data/A1.csv";
+        String B1 = "data/B1.csv";
 
         //classify the objects
         Classifier c = new Classifier(data, 90, 25);
